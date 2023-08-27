9647b849b023f2a3f75eb5e10d2c8a070a7ca027
IshaanChhabra <110290636+IshaanChhabra@users.noreply.github.com>
2023-08-27 19:12:49 +0530

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,18 +5,15 @@ function AboutUs() {
     <div className="about-container">
       <h1>Our History</h1>
       <p>
-        Vedika is owned and managed by Vedika Sharma, a successful engineer from
-        Thapar.
+        RIDS is owned and managed by Ishaan, a computer enginner from Thapar.
       </p>
       <h1>Our Mission</h1>
       <p>
-        Vedika is owned and managed by Vedika Sharma, a successful engineer from
-        Thapar.
+        Our aim is to curate exceptional travel experiences that cater to every traveler's unique desires, whether it's exploring hidden gems off the beaten path or indulging in luxurious escapes. With a commitment to sustainability and responsible tourism, we envision a future where our explorations leave a positive impact on the destinations we visit and the communities we engage with. Through personalized itineraries, expert guides, and a deep appreciation for the world's natural and cultural wonders, we strive to make every journey with us an unforgettable tale to be told.
       </p>
       <h1>Our Vision</h1>
       <p>
-        Vedika is owned and managed by Vedika Sharma, a successful engineer from
-        Thapar.
+        At RIDS, our vision is to inspire wanderlust and facilitate extraordinary journeys that transcend the ordinary. We believe that travel is not just about visiting new places; it's about immersing oneself in diverse cultures, forging connections, and creating lifelong memories. Join us in embracing the beauty of our planet and the endless possibilities it offers for exploration and enrichment. Your adventure of a lifetime begins here.
       </p>
     </div>
   );
